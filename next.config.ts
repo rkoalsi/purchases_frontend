190e3ffbbce894d0680062e5f4958f5c8e9d4ec2
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2025-05-24 15:17:21 +0530

    changing build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   env: {
     api_url: 'http://127.0.0.1:8000',
   },
