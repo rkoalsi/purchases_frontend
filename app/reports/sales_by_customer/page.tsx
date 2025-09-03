4d1278530eddec34913d31f02a5d2172b3ea08ac
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2025-09-03 21:31:59 +0530

    fixing check logic

--- app/reports/sales_by_customer/page.tsx
+++ app/reports/sales_by_customer/page.tsx
@@ -248,7 +248,7 @@ const InvoiceReportGenerator = () => {
                 id="default-checkbox"
                 type="checkbox"
                 name="exclude_customers"
-                // value={formData.exclude_customers}
+                checked={formData.exclude_customers}
                 onClick={handleInputChange}
                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               />
