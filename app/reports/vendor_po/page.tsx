ab23dc0d1deff2a4ffb28e74be947ab2da2fb707
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2026-04-24 11:36:06 +0530

    adding vendor central po

---
+++ app/reports/vendor_po/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useAuth } from '@/components/context/AuthContext';
+import VendorPOReport from '@/components/reports/VendorPOReport';
+import React from 'react';
+
+function Page() {
+  const { isLoading, accessToken } = useAuth();
+
+  if (isLoading) return <p>Loading user data...</p>;
+  if (!accessToken) return <p>Please log in to see this content.</p>;
+
+  return <VendorPOReport />;
+}
+
+export default Page;
